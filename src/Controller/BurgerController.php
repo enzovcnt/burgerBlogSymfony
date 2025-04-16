84ca4df1a09747cf19d91c7c540461be423835f5
Enzo <enzovcnt@gmail.com>
2025-04-16 15:07:41 +0200

    display the name of differents burgers

--- src/Controller/BurgerController.php
+++ src/Controller/BurgerController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\BurgerRepository;
+use http\Exception\BadUrlException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +11,13 @@ use Symfony\Component\Routing\Attribute\Route;
 final class BurgerController extends AbstractController
 {
     #[Route('/burger', name: 'burger')]
-    public function index(): Response
+    public function index(BurgerRepository $burgerRepository): Response
     {
+        $burgers = $burgerRepository->findAll();
+
         return $this->render('burger/index.html.twig', [
             'controller_name' => 'BurgerController',
+            'burgers' => $burgers,
         ]);
     }
 }
